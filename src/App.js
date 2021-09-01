53b3dbc73cf01bbdad56b54e89787e71ad0be59a
oromero270 <oromero2018@gmail.com>
2021-09-01 14:25:37 -0500

    The cart is functional and we can add items to it now.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import {commerce} from './lib/commerce';
 import {Products, Navbar} from './components';
 
 
+
 const App =() =>{
   const [products, setProducts] = useState([]);
-
+  const [cart, setCart] =useState ({});
+//This code is what fetches the products from the commerece API and I am allowed to destruicture the data in my app. 
   const fetchProducts= async () =>{
     const {data} = await commerce.products.list();
     setProducts(data);
   }
+//This code is what fetches the cart from the API
+  const fetchCart = async () =>{
+    const cart = await commerce.cart.retrieve();
+    setCart(cart)
+  }
+//This function handles the addition to the carts 
+  const handleAddToCart = async (productId,quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+//These are the functions that are fetched on the laod screen
   useEffect(() =>{
     fetchProducts();
+    fetchCart();
   },[]);
   console.log(products);
   return(
     <div>
-      <Navbar/>
-      <Products products={products}/>
+      <Navbar totalItems={cart.total_items}/>
+      <Products products={products} onAddToCart={handleAddToCart}/>
 
     </div>
   )
