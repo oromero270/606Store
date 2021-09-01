119be0c0d53e07e753f62b9a5e680a2e358bc2c0
oromero270 <oromero2018@gmail.com>
2021-09-01 11:13:00 -0500

    This is the addition of the navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
-
+import useStyles from './styles';
 const products =[
     {id:1, name:'Shoes',description:'running shoes',price:'$6' },
     {id:2, name:'flats',description:'running not shoes',price:'$8' },
 ]
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main >
-        <div  />
+        <main className={classes.content} >
+        <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
