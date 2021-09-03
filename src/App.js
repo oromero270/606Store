c6b9f50a0982f0b549f90fe685e3022691e20477
oromero270 <oromero2018@gmail.com>
2021-09-03 14:34:37 -0500

    Stripe has been set up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App =() =>{
   const [products, setProducts] = useState([]);
   const [cart, setCart] =useState ({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
 //This code is what fetches the products from the commerece API and I am allowed to destruicture the data in my app. 
   const fetchProducts= async () =>{
     const {data} = await commerce.products.list();
@@ -35,6 +38,19 @@ const App =() =>{
     const {cart} = await commerce.cart.empty();
     setCart(cart);
   }
+  const refreshCart= async () =>{
+    const newCart=await commerce.cartrefresh();
+    setCart(newCart);
+  }
+  const handleCaptureCheckout = async (checkoutTokenId,newOrder) =>{
+    try{
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+      setOrder(incomingOrder);
+      refreshCart();
+    }catch(error){
+      setErrorMessage(error.data.error.message)
+    }
+  }
 //These are the functions that are fetched on the laod screen
   useEffect(() =>{
     fetchProducts();
@@ -59,7 +75,7 @@ const App =() =>{
             />
           </Route>
           <Route exact path ='/checkout'>
-            <Checkout cart = {cart}/>
+            <Checkout cart = {cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>
           </Route>
         </Switch>
       </div>
