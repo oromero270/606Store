7cd3daf8250936de3dbb387365e9f040df4ad8bd
oromero270 <oromero2018@gmail.com>
2021-09-03 13:54:50 -0500

    Checkout Review working

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,20 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
-
+import { Link } from 'react-router-dom';
 import{commerce} from'../../lib/commerce';
 import FormInput from './CustomTextField';
 
-const AddressForm = ({checkoutToken}) => {
+
+const AddressForm = ({checkoutToken , next}) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
-    const [shippingSubDivisions, setShippingSubDivisions] = useState([]);
-    const [shippingSubDivision, setShippingSubDivision] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
     const [shippingOptions, setShippingOptions] = useState([]);
     const [shippingOption, setShippingOption] = useState('');
     const methods = useForm();
     const countries = Object.entries(shippingCountries).map(([code, name]) =>({id:code, label:name}))
-    
+    const options = shippingOptions.map((sO)=> ({id: sO.id, label: `${sO.description} - (${sO.price.formatted_with_symbol})`}))
     
     const fetchShippingCountries = async (checkoutTokenId) => {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
@@ -23,15 +24,35 @@ const AddressForm = ({checkoutToken}) => {
         setShippingCountry(Object.keys(countries)[0]);
     };
 
+    const fetchSubdivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+    
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions)[0]);
+    };
+
+    const fetchShippingOptions= async (checkoutTokenId, country, region = null) =>{
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId,{country, region});
+        setShippingOptions(options);
+        setShippingOption(options[0].id);
+    }
+
     useEffect(()=>{
         fetchShippingCountries(checkoutToken.id)
     },[]);
+
+    useEffect(() => {
+        if (shippingCountry) fetchSubdivisions(shippingCountry);
+    }, [shippingCountry]);
+    useEffect(() => {
+        if (shippingSubdivision) fetchShippingOptions(checkoutToken.id,shippingCountry,shippingSubdivision);
+    }, [shippingSubdivision]);
 
     return (
         <>
             <Typography variant="h6" gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit=''>
+                <form onSubmit={methods.handleSubmit((data)=> next({...data, shippingCountry, shippingSubdivision,setShippingOption}) )}>
                     <Grid container spacing={3}>
                         <FormInput required name="firstName" label="First name" />
                         <FormInput required name="lastName" label="Last name" />
@@ -50,23 +71,32 @@ const AddressForm = ({checkoutToken}) => {
                             
                             </Select>
                         </Grid>
-                        {/* <Grid item xs={12} sm={6}>
-                            <InputLabel>Shipping SuDivision</InputLabel>
-                            <Select value={} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
-                                    Select me
-                                </MenuItem>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                                <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                                    {Object.entries(shippingSubdivisions).map(([code, name]) => ({ id: code, label: name })).map((item) => (
+                                    <MenuItem key={item.id} value={item.id}>
+                                        {item.label}
+                                    </MenuItem>
+                                        ))}
                             </Select>
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Options</InputLabel>
-                            <Select value={} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
-                                    Select me
-                                </MenuItem>
+                            <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                                {options.map((option) =>(
+                                    <MenuItem key={option.id} value={option.id}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
                             </Select>
-                        </Grid> */}
+                        </Grid>
                     </Grid>
+                    <br />
+                    <div style={{display:'flex', justifyContent:'space-between'}}>
+                                    <Button component={Link} to='/cart' varient='outlined'>Back to Cart</Button>
+                                    <Button varient='contained' type='submit' color='primary'>Next</Button>
+                    </div>
                 </form>
             </FormProvider>
         </>
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
-const PaymentForm = () => {
+import React from 'react';
+import { Typography,Button, Divider } from '@material-ui/core';
+import{Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review'
+const PaymentForm = ({checkoutToken}) => {
     return (
         <div>
-            
-            payment form
+            <Review checkoutToken={checkoutToken}>
+
+            </Review>
         </div>
     )
 }
